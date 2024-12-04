6085b5ed1a9db0299fed273782410c52d238515f
donkim <donkim@gmail.com>
2024-12-05 03:52:20 +0900

    장바구니 화면 구현

--- front/src/context/StoreContext.jsx
+++ front/src/context/StoreContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
@@ -21,15 +21,28 @@ export const StoreContextProvider = (props) => {
         setCartItems((prev)=>({...prev, [itemId]:prev[itemId]-1}))
     }
 
-    useEffect(()=>{
-        console.log(cartItems);
-    }, [cartItems]);
+    const getTotalCartAmount = () =>{
+        let totalAmount = 0;
+        for(const item in cartItems){
+            if(cartItems[item] > 0){
+                let itemInfo = food_list.find((p)=>p._id === item);
+                totalAmount += itemInfo.price * cartItems[item];
+            }
+            
+            
+        }
+        return totalAmount;
+    }
+    // useEffect(()=>{
+    //     console.log(cartItems);
+    // }, [cartItems]);
     const contextValue = {
         food_list,
         cartItems,
         setCartItems,
         addToCart,
-        removeFromCart
+        removeFromCart,
+        getTotalCartAmount
     }
     return (
         <StoreContext.Provider value={contextValue}>
