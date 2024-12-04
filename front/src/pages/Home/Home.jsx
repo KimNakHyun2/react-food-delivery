15cd164d924b9f39e200098bb34f4a5a5c60104c
donkim <donkim@gmail.com>
2024-12-04 21:32:44 +0900

    Footer와 OS별 다운로드 추가

--- front/src/pages/Home/Home.jsx
+++ front/src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import AppDownload from '../../components/AppDownload/AppDownload'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 import Header from '../../components/Header/Header'
@@ -6,11 +7,13 @@ import "./Home.css"
 
 const Home = () => {
     const [category, setCategory] = useState("All");
+
   return (
     <div>
         <Header/>
         <ExploreMenu category={category} setCategory={setCategory}/>
         <FoodDisplay category={category} />
+        <AppDownload/>
     </div>
   )
 }
